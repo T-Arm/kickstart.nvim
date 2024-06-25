ca57c4c658c3863ecb9efab2ba4465ccd1f6349e
tdarmstrong <armstrtd@lafayette.edu>
2024-06-25 00:10:01 -0400

    First stable state. Added many features including working lsps and better themeing

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Set concavelevel for obidian plugin
+vim.opt.conceallevel = 1
+
 -- Make line numbers default
 -- vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -566,8 +569,8 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        clangd = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -779,16 +782,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'olimorris/onedarkpro.nvim',
+    priority = 10000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
+      vim.cmd 'colorscheme onedark'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      require('onedarkpro').setup {
+        colors = {
+          Comment = 'DDDDDD',
+        },
+      }
     end,
   },
 
@@ -886,7 +893,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
